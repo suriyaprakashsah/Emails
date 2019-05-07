463ed087680e14dfd734cd8ab2603cca8c1a1ecf
suriya prakash sah <suriya.prakash64@gmail.com>
2019-05-07 11:21:35 +0530

    added new file

---
+++ Math syntax.py
@@ -0,0 +1,151 @@
+#abs
+#print ("abs(-45) : ", abs(-45))
+#print ("abs(100.12) : ", abs(100.12))
+
+#math Module
+#import math   # This will import math module
+
+#print ("math.ceil(-45.17) : ", math.ceil(-45.17))
+#print ("math.ceil(100.12) : ", math.ceil(100.12))
+#print ("math.ceil(100.72) : ", math.ceil(100.72))
+#print ("math.ceil(math.pi) : ", math.ceil(math.pi))
+
+#exp in python
+#This method returns exponential of x: ex.
+#import math   # This will import math module
+#print ("math.exp(-45.17) : ", math.exp(-45.17))
+#print ("math.exp(100.12) : ", math.exp(100.12))
+#print ("math.exp(100.72) : ", math.exp(100.72))
+#print ("math.exp(math.pi) : ", math.exp(math.pi))
+
+#This method returns the absolute value of x.
+# #import math   # This will import math module
+
+#print ("math.fabs(-45.17) : ", math.fabs(-45.17))
+#print ("math.fabs(100.12) : ", math.fabs(100.12))
+#print ("math.fabs(100.72) : ", math.fabs(100.72))
+#print ("math.fabs(math.pi) : ", math.fabs(math.pi))
+
+#The following example shows the usage of the floor() method.
+#import math   # This will import math module
+
+#print ("math.floor(-45.17) : ", math.floor(-45.17))
+#print ("math.floor(100.12) : ", math.floor(100.12))
+#print ("math.floor(100.72) : ", math.floor(100.72))
+#print ("math.floor(math.pi) : ", math.floor(math.pi))
+
+#The following example shows the usage of the log() method.
+#import math   # This will import math module
+
+#print ("math.log(100) : ", math.log(100))
+#print ("math.log(100.72) : ", math.log(100.72))
+#print ("math.log(math.pi) : ", math.log(math.pi))
+
+#Following is the syntax for log10() method −
+#import math   # This will import math module
+#print ("math.log10(100) : ", math.log10(100))
+#print ("math.log10(100.72) : ", math.log10(100.72))
+#print ("math.log10(119) : ", math.log10(119))
+#print ("math.log10(math.pi) : ", math.log10(math.pi))
+
+#For Maximum Numbers
+#print ("max(80, 100, 1000) : ", max(80, 100, 1000))
+#print ("max(-20, 100, 400) : ", max(-20, 100, 400))
+#print ("max(-80, -20, -10) : ", max(-80, -20, -10))
+#print ("max(0, 100, -400) : ", max(0, 100, -400))
+
+#For Minimum Numbers
+#print ("min(80, 100, 1000) : ", min(80, 100, 1000))
+#print ("min(-20, 100, 400) : ", min(-20, 100, 400))
+#print ("min(-80, -20, -10) : ", min(-80, -20, -10))
+#print ("min(0, 100, -400) : ", min(0, 100, -400))
+
+#The following example shows the usage of the modf() method.
+#import math   # This will import math module
+#print ("math.modf(100.12) : ", math.modf(100.12))
+#print ("math.modf(100.72) : ", math.modf(100.72))
+#print ("math.modf(119) : ", math.modf(119))
+#print ("math.modf(math.pi) : ", math.modf(math.pi))
+
+#The following example shows the usage of the pow() method.
+#import math   # This will import math module
+#print ("math.pow(100, 2) : ", math.pow(100, 2))
+#print ("math.pow(100, -2) : ", math.pow(100, -2))
+#print ("math.pow(2, 4) : ", math.pow(2, 4))
+#print ("math.pow(3, 0) : ", math.pow(3, 0))
+
+#round function in python
+#print ("round(70.23456) : ", round(70.23456))
+#print ("round(56.659,1) : ", round(56.659,1))
+#print ("round(80.264, 2) : ", round(80.264, 2))
+#print ("round(100.000056, 3) : ", round(100.000056, 3))
+#print ("round(-100.000056, 3) : ", round(-100.000056, 3))
+
+#The following example shows the usage of sqrt() method.
+#import math   # This will import math module
+#print ("math.sqrt(100) : ", math.sqrt(100))
+#print ("math.sqrt(7) : ", math.sqrt(7))
+#print ("math.sqrt(math.pi) : ", math.sqrt(math.pi))
+
+#This method returns a random item.
+#import random
+#print ("returns a random number from range(100) : ",random.choice(range(100)))
+#print ("returns random element from list [1, 2, 3, 5, 9]) : ", random.choice([1, 2, 3, 5, 9]))
+#print ("returns random character from string 'Hello World' : ", random.choice('Hello World'))
+
+#The following example shows the usage of the randrange() method.
+#import random
+# randomly select an odd number between 1-100 
+#print ("randrange(1,100, 2) : ", random.randrange(1, 100, 2))
+# randomly select a number between 0-99
+#print ("randrange(100) : ", random.randrange(100))
+
+#The following example shows the usage of sin() method.
+#import math
+#print ("sin(3) : ",  math.sin(3))
+#print ("sin(-3) : ",  math.sin(-3))
+#print ("sin(0) : ",  math.sin(0))
+#print ("sin(math.pi) : ",  math.sin(math.pi))
+#print ("sin(math.pi/2) : ",  math.sin(math.pi/2))
+
+#The following example shows the usage of cos() method.
+#import math
+#print ("cos(3) : ",  math.cos(3))
+#print ("cos(-3) : ",  math.cos(-3))
+#print ("cos(0) : ",  math.cos(0))
+#print ("cos(math.pi) : ",  math.cos(math.pi))
+#print ("cos(2*math.pi) : ",  math.cos(2*math.pi))
+
+#The following example shows the usage of tan() method.
+#import math
+#print ("(tan(3) : ",  math.tan(3))
+#print ("tan(-3) : ",  math.tan(-3))
+#print ("tan(0) : ",  math.tan(0))
+#print ("tan(math.pi) : ",  math.tan(math.pi))
+#print ("tan(math.pi/2) : ",  math.tan(math.pi/2))
+#print ("tan(math.pi/4) : ",  math.tan(math.pi/4))
+
+#This method returns degree value of an angle.
+#import math
+#print ("degrees(3) : ",  math.degrees(3))
+#print ("degrees(-3) : ",  math.degrees(-3))
+#print ("degrees(0) : ",  math.degrees(0))
+#print ("degrees(math.pi) : ",  math.degrees(math.pi))
+#print ("degrees(math.pi/2) : ",  math.degrees(math.pi/2))
+#print ("degrees(math.pi/4) : ",  math.degrees(math.pi/4))
+
+
+#This method returns radian value of an angle.
+#import math
+#print ("radians(3) : ",  math.radians(3))
+#print ("radians(-3) : ",  math.radians(-3))
+#print ("radians(0) : ",  math.radians(0))
+#print ("radians(math.pi) : ",  math.radians(math.pi))
+#print ("radians(math.pi/2) : ",  math.radians(math.pi/2))
+#print ("radians(math.pi/4) : ",  math.radians(math.pi/4))
+
+#This method returns Euclidean norm, sqrt(x*x + y*y).
+#import math
+#print ("hypot(3, 2) : ",  math.hypot(3, 2))
+#print ("hypot(-3, 3) : ",  math.hypot(-3, 3))
+#print ("hypot(0, 2) : ",  math.hypot(0, 2))
